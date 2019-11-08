d35d5477ca795d63516d21c9d06dbdd5c035e4d2
Dmitry Peredera <peredera@gmail.com>
2019-11-09 02:13:23 +0600

    start for implementation Observer pattern

---
+++ Observer/Observer/Bank.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Observer
+{
+	internal class Bank : IObservable
+	{
+		private readonly List<IObserver> _observers;
+
+		public Bank()
+		{
+			_observers = new List<IObserver>();
+		}
+
+		public void RegisterObserver(IObserver observer)
+		{
+			if (!_observers.Contains(observer))
+				_observers.Add(observer);
+		}
+
+		public void RemoveObserver(IObserver observer)
+		{
+			if (_observers.Contains(observer))
+				_observers.Remove(observer);
+		}
+
+		public void Notify()
+		{
+			foreach (var observer in _observers)
+			{
+				observer.Update("hallalah!");
+			}
+		}
+	}
+}
---
+++ Observer/Observer/IObservable.cs
@@ -0,0 +1,9 @@
+﻿namespace Observer
+{
+	internal interface IObservable
+	{
+		void RegisterObserver(IObserver observer);
+		void RemoveObserver(IObserver observer);
+		void Notify();
+	}
+}
